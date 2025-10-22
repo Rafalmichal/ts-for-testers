d9e77bad3c48d3ed0024230804483b8e2e2161f9
Rafalb <rafalmichalblaszczuk@gmail.com>
2025-10-22 23:19:01 +0200

    Praca z tekstem

--- src/course/M03/M03L05-string.ts
+++ src/course/M03/M03L05-string.ts
@@ -23,3 +23,29 @@ const lastName = 'testowy';
 console.log('My name is ' + firstName + ' ' + lastName);
 // template string , string literals
 console.log(`My name is ${firstName} ${lastName}`);
+
+//zamiana stringów
+// na wielkie i male litery
+console.log(firstName.toUpperCase());
+console.log(firstName.toLowerCase());
+
+//zamiana znaków/słów
+const text = 'Hello, I am learning TypeScript';
+console.log(text.replace('TypeScript', 'JavaScript'));
+console.log(text.replaceAll(' ', '_'));
+
+//powtarzanie stringów
+const laugh = 'ha';
+console.log(laugh.repeat(5));
+
+//długość stringa
+console.log(laugh.length);
+
+// dopełnienie stringa w jednej linii
+// najpierw dodaj 5 '*' z przodu (wydłuża string do length + 5), potem dopełnij do length + 10, żeby dodać 5 '!'
+console.log(firstName.padStart(firstName.length + 5, '*').padEnd(firstName.length + 10, '!'));
+
+//usuwanie białych znaków
+const titleFromWebsite = '   Learn TypeScript !  ';
+console.log(titleFromWebsite);
+console.log(titleFromWebsite.trim());
